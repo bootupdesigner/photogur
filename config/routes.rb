c6e7554f531cdd52fd6f5b2855cdab426a9908f1
bootupdesigner <bootup.itinfo@gmail.com>
2021-04-29 18:28:02 -0400

    defined login routes to correct error

--- config/routes.rb
+++ config/routes.rb
@@ -8,12 +8,13 @@ resources :users, only: [:create, :show]
   get "/login", to: "sessions#new"
   post "/sessions", to: "sessions#create"
   delete "/sessions", to: "sessions#destroy"
+  get '/logout', to: 'sessions#destroy'
 
 get 'users/new' => 'users#new', as: :new_user
 	
 post 'users' => 'users#create'
 
-get '/login'     => 'sessions#new'
+# get '/login'     => 'sessions#new'
 	
 post '/login'    => 'sessions#create'
 	
